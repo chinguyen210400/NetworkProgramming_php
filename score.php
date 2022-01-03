81eb511849b0dc03a6525a53ddbe7fddc4061dc3
nguyenchi <chi.ny184049@sis.hust.edu.vn>
2022-01-04 01:52:37 +0700

    add score style

--- score.php
+++ score.php
@@ -1,5 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+<style>
+    .score_submit {
+        background-color: #1961aa;
+        color:white;
+        padding: 8px 20px;
+        border: 1px solid #0c335a;
+        transition: all 0.3s ease-out;
+        border-radius: 10px;
+        margin-bottom: 15px;
+        padding: 8px 10px;
+        font-size: 20px;
+        font-family: Arial, sans-serif;
+        width: 400px;
+        height: 60px;
+        margin-left: 100px;
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
+
+    .score_submit:hover {
+        background-color: white;
+        color:#0c335a;
+        border: 1px solid #1961aa;
+    }
+
+    .score_display {
+        background-color: #ffffff;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        height: 600px;
+        width: 600px;
+        margin-top : 50px;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .score_display h5{
+        font-size:50px;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
 
 <head>
     <meta charset="UTF-8">
@@ -39,15 +82,17 @@
 <body>
     <?php include('home_navbar.php'); ?>
     <div class="home_container">
-        <div class="index_image">
-    <img src="./prjltm.jpg" class="rounded mx-auto d-block " alt="index.php" >
+<div class=" d-flex justify-content-center"> 
+    <div class="score_contain  list-group">
+<div class="score_display">
+        <h5>YOUR SCORE: <?php echo $_SESSION["position"];?> </h5>
 </div>
-<div class="score_display ">
-<?php echo $_SESSION["position"];?>
-</div>
+ <div class="score_button">
 <form action="home.php" method="post">
-        <input type="submit" class="btn  btn-lg btn-outline-primary mt-4 " name ="home" value="OK" >
+        <input type="submit" class="score_submit" name ="home" value="OK" >
     </form>
+</div>
+</div>
 </div>
     <?php include('footer.php'); ?>
 </body>
